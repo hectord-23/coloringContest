1c67f8fba6831e0ee69c21bd0a4a35cfbdbf4ae8
Hector Diaz <hectord@uw.edu>
2016-03-01 19:27:41 -0800

    A Test class, finally got everything working. Had to download the newest version of eclipse.

---
+++ Final Project/src/model/Test.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test class, added by Hector, Will be delted later.");
+	}
+
+}
