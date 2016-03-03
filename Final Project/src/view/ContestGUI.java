67d08240a0f2f5975b0c8351f3ad5865d18fee36
Jonahk <jonahkh@uw.edu>
2016-03-03 11:30:08 -0800

    Changed GUI

--- Final Project/src/view/ContestGUI.java
+++ Final Project/src/view/ContestGUI.java
@@ -1,9 +1,22 @@
 package view;
 
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
-
+	
 public class ContestGUI extends JFrame {
+	
+	public ContestGUI() {
+		super("TCSS 360 - Fist Full of Java");
+		final Toolkit kit = Toolkit.getDefaultToolkit();
+		setLocation((int) (kit.getScreenSize().getWidth() / 2 - getWidth() / 2),
+				(int) (kit.getScreenSize().getHeight() / 2 - getHeight() / 2));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1000, 1000);
+		setVisible(true);
+	}
 
-	
-	
+	public static void main(String... theArgs) {
+		ContestGUI gui = new ContestGUI();
+	}
 }
