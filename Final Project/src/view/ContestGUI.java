873fec7ae5cae64d273309674f0e538e97bfaa70
Jonahk <jonahkh@uw.edu>
2016-03-03 12:25:20 -0800

    Added stuff to introgui

--- Final Project/src/view/ContestGUI.java
+++ Final Project/src/view/ContestGUI.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -8,11 +10,14 @@ public class ContestGUI extends JFrame {
 	
 	public ContestGUI() {
 		super("TCSS 360 - Fist Full of Java");
+		setLayout(new CardLayout());
+		setBackground(Color.WHITE);
 		final Toolkit kit = Toolkit.getDefaultToolkit();
 		setLocation((int) (kit.getScreenSize().getWidth() / 2 - getWidth() / 2),
 				(int) (kit.getScreenSize().getHeight() / 2 - getHeight() / 2));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1000, 1000);
+		add(new IntroPanel());
 		setVisible(true);
 	}
 
--- Final Project/src/view/IntroPanel.java
+++ Final Project/src/view/IntroPanel.java
@@ -1,7 +1,53 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class IntroPanel extends JPanel {
+	
+	public IntroPanel() {
+		super();
+		setUpComponents();
+	}
+	
+	private void setUpComponents() {
+		final JPanel northPanel = new JPanel();
+		JLabel label = new JLabel("Clark County Library Coloring Contest");
+		label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 45));
+		northPanel.add(label);
+		add(northPanel, BorderLayout.NORTH);
+		
+		final JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.X_AXIS));
+		final JPanel details = getDetails();
+		add(details, BorderLayout.CENTER);
+		final JPanel buttons = getButtonsPanel();
+	}
 
+	private JPanel getButtonsPanel() {
+		return null;
+	}
+	private JPanel getDetails() {
+		final JPanel panel = new JPanel();
+		try {
+			Scanner input = new Scanner(new File("./extras/Coloring_Contest_Details"));
+			JLabel label = new JLabel();
+			StringBuilder s = new StringBuilder();
+			while (input.hasNextLine()) {
+				s.append(input.nextLine() + "\n");
+			}
+			panel.add(new JLabel(s.toString()));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found");
+		}
+		return panel;
+	}
 }
