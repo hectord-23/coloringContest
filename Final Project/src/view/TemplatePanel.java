661241429d262c3591f9744e2ebb6967d54e8624
Jonah <Jonah@Jonah-PC.hsd1.wa.comcast.net>
2016-03-08 18:47:03 -0800

    added comments

--- Final Project/src/view/TemplatePanel.java
+++ Final Project/src/view/TemplatePanel.java
@@ -10,9 +10,10 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
+
 import java.io.File;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,14 +33,36 @@ import javax.swing.JScrollPane;
 
 import model.TemplateDB;
 
+/**
+ * This class represents the template panel. Contestants can choose from a list of images
+ * and download one or many to their system. Images must be in .jpg format.
+ * 
+ * @author Jonah Howard
+ * @version 8 March 2016
+ */
 public class TemplatePanel extends Observable {
+	
+	/** The current panel holding all contents for this page/card. */
 	private final JPanel myPanel;
+	
+	/** The list of templates. */
 	private final List<ImageIcon> myTemplates;
+	
+	/** Scroll pane for the list of templates, allows it to be scrollable. */
 	private final JScrollPane myScrollPane;
+	
+	/** List of all buttons that display an image. */
 	private final List<JButton> myButtons;
+	
+	/** Maps a button to its full sized image. */
 	private final Map<JButton, ImageIcon> myIcons;
+	
+	/** The current file chooser. */
 	private final JFileChooser myFileChooser;
 	
+	/**
+	 * Initialize a new Template Panel.
+	 */
 	public TemplatePanel() {
 		myFileChooser = new JFileChooser(new File("./extras/Downloads"));
 		myIcons = new HashMap<JButton, ImageIcon>();
@@ -67,20 +90,31 @@ public class TemplatePanel extends Observable {
 		
 	}
 	
+	/**
+	 * Get the list of buttons that are associated with the images. Each image is a button
+	 * and when pressed prompts the user to save it to their system.
+	 * 
+	 * @return list of the templates/buttons
+	 */
 	private List<JButton> getButtons() {
 		final List<JButton> list = new ArrayList<JButton>();
 		for (final ImageIcon current : myTemplates) {
 			final Image img = current.getImage();
+			// shrink the image to fit on a button
 			final ImageIcon smallImg = new ImageIcon(img.getScaledInstance(150, 150, Image.SCALE_DEFAULT));
 			final JButton button = new JButton(smallImg);
 			list.add(button);
 			myIcons.put(button, current);
 
 		}
-		
 		return list;
 	}
 	
+	/**
+	 * Returns the panel that contains all of the templates. 
+	 * 
+	 * @return The panel containing all of the images
+	 */
 	private JPanel getImages() {
 		final JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout((int) Math.ceil((double) myTemplates.size() / 3), 3, 5, 20));
@@ -94,12 +128,12 @@ public class TemplatePanel extends Observable {
 					myFileChooser.showSaveDialog(myPanel);
 					File f = myFileChooser.getSelectedFile();
 					Image img = myIcons.get(button).getImage();
-					BufferedImage bi = (BufferedImage) img;//new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+					BufferedImage bi = (BufferedImage) img;
 					Graphics g = bi.getGraphics();
 					g.drawImage(bi, 0, 0, null);
 
 					try {
-						ImageIO.write(bi, "jpg", f);// new File("extras/Downloads/template.jpg"));
+						ImageIO.write(bi, "jpg", f);
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
